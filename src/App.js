cafa8c08377e303680e48cdc9d2a951caedc178e
rayw478 <raymondw478@gmail.com>
2021-05-06 01:33:33 -0700

    navbar complete

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
-      </Router>
-    </>
+    <Navbar />
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { Component, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MenuItems } from "./MenuItems"
+import "./Navbar.css"
+import { Button } from "../Button"
 
-function Navbar() {
-    return (
-        <>
+class Navbar extends Component {
+
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render() {
+        return (
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React</h1>
-                <div className="menu-icon">
-
+                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
@@ -21,9 +30,10 @@ function Navbar() {
                         )
                     })}
                 </ul>
+                <Button>Sign Up</Button>
             </nav>
-        </>
-    )
+        )
+    }
 }
 
 export default Navbar
